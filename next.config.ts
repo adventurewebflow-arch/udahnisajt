44dc1868de1a427f28ef33f4e7bc04f75e44a124
Petar <udahniavanturu@gmail.com>
2026-02-24 20:14:07 +0100

    Add 301 redirects from old Webflow URLs

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,45 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  async redirects() {
+    return [
+      {
+        source: "/camping/trnovackojezero-maglic",
+        destination: "/ture/kampovanje-trnovacko-jezero-maglic",
+        permanent: true,
+      },
+      {
+        source: "/camping/ljetna-avantura-na-tjentistu",
+        destination: "/ture/porodicni-vikend-tjentiste-zelengora",
+        permanent: true,
+      },
+      {
+        source: "/camping/zelengora-jahanje-konja",
+        destination: "/ture/kampovanje-zelengora-bregoč-jeep-safari",
+        permanent: true,
+      },
+      {
+        source: "/izleti/kanjoning-ili-trnovacko-jezero",
+        destination: "/ture/kanjoning-hrcavka",
+        permanent: true,
+      },
+      {
+        source: "/specijalne-ponude/kreiraj-svoju-avanturu",
+        destination: "/ture/kanjoning-hrcavka-prasuma-perucica-skakavac",
+        permanent: true,
+      },
+      {
+        source: "/faq",
+        destination: "/",
+        permanent: true,
+      },
+      {
+        source: "/contact",
+        destination: "/#kontakt-form",
+        permanent: true,
+      },
+    ];
+  },
 };
 
 export default nextConfig;
